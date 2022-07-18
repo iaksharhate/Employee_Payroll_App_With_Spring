fa9092bfeb52e14f89f21abe43eeb5b98dc4715c
iaksharhate <iaksharhate@gmail.com>
2022-07-19 01:31:01 +0530

    Added: Lombok Library for Logging using @Slf4j.

--- src/main/java/com/example/employeepayrollappspring/EmployeePayrollAppSpringApplication.java
+++ src/main/java/com/example/employeepayrollappspring/EmployeePayrollAppSpringApplication.java
@@ -1,13 +1,16 @@
 package com.example.employeepayrollappspring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollAppSpringApplication.class, args);
         System.out.println("Welcome to Employee Payroll App.");
+        log.info("Employee Payroll App Started.");
     }
 }
