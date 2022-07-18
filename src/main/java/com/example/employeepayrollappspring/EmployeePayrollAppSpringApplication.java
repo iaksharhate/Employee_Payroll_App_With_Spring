cebb6f2d694d499c77163422e36a3b19531fcfb6
iaksharhate <iaksharhate@gmail.com>
2022-07-19 02:34:12 +0530

    Added: Setting Up Dev Environment.

--- src/main/java/com/example/employeepayrollappspring/EmployeePayrollAppSpringApplication.java
+++ src/main/java/com/example/employeepayrollappspring/EmployeePayrollAppSpringApplication.java
@@ -3,14 +3,17 @@ package com.example.employeepayrollappspring;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollAppSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppSpringApplication.class, args);
+        ApplicationContext context = SpringApplication
+                                    .run(EmployeePayrollAppSpringApplication.class, args);
         System.out.println("Welcome to Employee Payroll App.");
-        log.info("Employee Payroll App Started.");
+        log.info("Employee Payroll App Started in {} Environment.",
+                            context.getEnvironment().getProperty("environment"));
     }
 }
